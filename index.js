36ac7d2108c267200fdd828e38d2d72ffa6ba256
umerch1 <umerf3024@gmail.com>
2024-08-22 20:00:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// let a = "            Hello world, How are you?             ";
+// let b = "Type here to search";
+
+// console.log(a);
+
+// console.log(a.concat(" ", b));
+
+// console.log(a + " " + b);
+
+// console.log(a.trim());
+
+const str = "Hello world";
+
+// console.log(str.length);
+// console.log(str.charAt(6));
+
+// console.log("Code check" + str.charCodeAt(0));
+// console.log(str.split(" "));
+
+// let sym1 = Symbol("a");
+// let sym2 = Symbol("b");
+// let sym3 = Symbol("a");
+
+// console.log("Check sym1" + (sym1 === sym3));
+
+let num = 6.545646824647889;
+
+console.log(num.toFixed());
